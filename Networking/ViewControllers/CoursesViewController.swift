628398c42aa2c3acda464bef17a3eb8c0bdeb22e
Alexey <alexeyradomskiy98@gmail.com>
2022-03-16 20:10:21 +0300

    Setup ViewControllers

---
+++ Networking/ViewControllers/CoursesViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CoursesViewController.swift
+//  Networking
+//
+//  Created by Alexey on 16.03.2022.
+//
+
+import UIKit
+
+class CoursesViewController: UITableViewController {
+    
+    private var courses: [Course] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 100
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        courses.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CourseCell
+        let course = courses[indexPath.row]
+        cell.configure(with: course)
+        return cell
+    }
+}
+
+// MARK: - Networking
+extension CoursesViewController {
+    
+    func fetchCourses() {
+        guard let url = URL(string: Link.exampleTwo.rawValue) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                self.courses = try JSONDecoder().decode([Course].self, from: data)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
