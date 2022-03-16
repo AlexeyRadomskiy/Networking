f70675f034d4ebc62a1945356fa2820d636ff566
Alexey <alexeyradomskiy98@gmail.com>
2022-03-16 20:09:12 +0300

    Setup Storyboards

---
+++ Networking/UserActionCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserActionCell.swift
+//  Networking
+//
+//  Created by Alexey on 16.03.2022.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+}
